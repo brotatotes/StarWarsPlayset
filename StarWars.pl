af68c72fa972d6c0c9ff14ab857263937482d9f8
brotatotes <hao.awesome@gmail.com>
2016-04-18 22:56:14 -0500

    added sample playset and started starwars playset with 40 basic items.

---
+++ StarWars.pl
@@ -0,0 +1,51 @@
+%% EECS 395
+%% Assignment 3
+%% Star Wars Theme
+%% Eric Hao
+%% LuoLei Zhao
+
+
+
+roles_relation(secret_parent/unknowing_child).
+roles_relation(bounty_hunter/target).
+roles_relation(master/apprentice).
+roles_relation(sith_lord/evil_minion).
+roles_relation(rebel_commander/rebel_soldier).
+roles_relation(droid/owner).
+roles_relation(stormtrooper_unit_1/stormtrooper_unit_2).
+roles_relation(imperial/rebel).
+roles_relation(alien_crime_lord/bounty_hunter).
+roles_relation(clumsy_galaxy_food_deliverer/customer).
+
+need(bring_balance_to_the_force).
+need(crush_the_rebellion).
+need(destroy_death_star).
+need(escape_from_bounty_hunters_or_pay_off_debts).
+need(realize_potential_as_jedi).
+need(hunt_for_bounty).
+need(deliver_secret_message_to_rebels).
+need(make_quick_and_dirty_fortune).
+need(convert_everyone_to_dark_side).
+need(convert_your_son_to_dark_side).
+
+object(lightsaber).
+object(dl_44_blaster_pistol).
+object(secret_plans_to_the_death_star).
+object(x_wing_starfighter).
+object(broken_r2_series_droid).
+object(locked_chest_only_the_force_can_open).
+object(han_solo_frozen_in_carbonite).
+object(controls_to_a_planet_destroying_laser).
+object(bag_of_live_midi-chlorians).
+object(container_of_bacta).
+
+location(heart_of_the_death_star).
+location(middle_of_an_asteroid_field).
+location(stranded_on_dagobah).
+location(prison_in_Jabba's_Palace).
+location(busy_street_in_cloud_city).
+location(mos_eisley_cantina).
+location(command_bridge_of_rebel_space_cruiser).
+location(secret_factory_on_sullust).
+location(audience_at_pod_race).
+location(pit_of_carkoon_execution_platform).
