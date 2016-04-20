28cd7058ff9cd6fe61ba34fafefa3889f6617f8a
LuoLei Zhao <larryzhao740@yahoo.com>
2016-04-19 20:35:56 -0500

    Added implies

--- StarWars.pl
+++ StarWars.pl
@@ -5,29 +5,75 @@
 %% LuoLei Zhao
 
 
-
+conflicting_roles([politician, strategist, staffer, opposition_researcher, intern,
+		   lobbyist, journalist, billionaire, foreign_dictator, televangelist, activist,
+		   special_prosecutor, supreme_court_justice, lawyer,
+		   drug_dealer, madam, hired_burgler, hacker, honey_trap, 'PR strategist',
+		   militia_member ]).
+		   
 roles_relation(secret_parent/unknowing_child).
 roles_relation(bounty_hunter/target).
 roles_relation(master/apprentice).
+	implies(role(X,master),role(X,force_sensitive))
+	implies(role(X,apprentice,role(X,force_sensitive))
 roles_relation(sith_lord/evil_minion).
+	implies(role(X, sith_lord),
+		role(X, imperial)).
+	implies(role(X, evil_minion),
+		role(X, imperial)).
+	implies(role(X,sith_lord),
+		role(X,force_sensitive))
 roles_relation(rebel_commander/rebel_soldier).
-roles_relation(droid/owner).
-roles_relation(stormtrooper_unit_1/stormtrooper_unit_2).
+	implies(role(X, rebel_soldier),
+		role(X, rebel)).
+	implies(role(X, rebel_commander),
+		role(X, rebel)).
+	roles_relation(droid/owner).
+symmetric(stormtroopers_in_same_unit).
+	implies(relationship(X, stormtroopers_in_same_unit, _),
+		role(X, imperial)).
+	implies(relationship(_, stormtroopers_in_same_unit, Y),
+		role(Y, imperial)).
+		
+roles_relation(force_sensitive/skeptic_of_the_force).
+conflicting_roles(force_sensitive,skeptic_of_the_force).
+	
 roles_relation(imperial/rebel).
+	conflicting_roles(imperial, rebel).
+
 roles_relation(alien_crime_lord/bounty_hunter).
 roles_relation(clumsy_galaxy_food_deliverer/customer).
 
+%
+% Needs
+%
+need(get_revenge_on(X)) :-
+	rival(X).
+rival(enemy_commander).
+rival(sith_lord).
+rival(bounty_hunter).
+drug(alien_crime_lord).
+
 need(bring_balance_to_the_force).
 need(crush_the_rebellion).
-need(destroy_death_star).
-need(escape_from_bounty_hunters_or_pay_off_debts).
-need(realize_potential_as_jedi).
-need(hunt_for_bounty).
-need(deliver_secret_message_to_rebels).
-need(make_quick_and_dirty_fortune).
-need(convert_everyone_to_dark_side).
-need(convert_your_son_to_dark_side).
+role_need(imperial, crush_the_rebellion).
+role_need(apprentice, realize_potential_as_jedi).
+role_need(rebel, deliver_secret_message_to_rebels).
+role_need(target, escape_from_old_debts_to_crime_lord).
 
+need(destroy_death_star).
+contradiction(needs(C, destroy_death_star),
+	      role(C, imperial)).
+role_need(bounty_hunter,hunt_for_bounty).
+need(make_quick_and_dirty_fortune).
+contradiction(needs(C, destroy_death_star),
+	      role(C, sith_lord)).
+role_need(sith_lord,convert_everyone_to_dark_side).
+
+
+%
+% Objects
+%
 object(lightsaber).
 object(dl_44_blaster_pistol).
 object(secret_plans_to_the_death_star).
@@ -36,10 +82,11 @@ object(broken_r2_series_droid).
 object(locked_chest_only_the_force_can_open).
 object(han_solo_frozen_in_carbonite).
 object(controls_to_a_planet_destroying_laser).
+	implies( has(X,controls_to_a_planet_destroying_laser), at(X,inside_the_death_star))
 object(bag_of_live_midi-chlorians).
 object(container_of_bacta).
 
-location(heart_of_the_death_star).
+location(inside_the_death_star).
 location(middle_of_an_asteroid_field).
 location(stranded_on_dagobah).
 location(prison_in_Jabbas_Palace).
