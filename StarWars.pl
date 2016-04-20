d003c83a88b49d25d881ceb7986176c7d0caa347
brotatotes <hao.awesome@gmail.com>
2016-04-19 22:21:43 -0500

    added more conflicts, unique roles, additional rules

--- StarWars.pl
+++ StarWars.pl
@@ -4,13 +4,9 @@
 %% Eric Hao
 %% LuoLei Zhao
 
-
-%% conflicting_roles([politician, strategist, staffer, opposition_researcher, intern,
-%% 		   lobbyist, journalist, billionaire, foreign_dictator, televangelist, activist,
-%% 		   special_prosecutor, supreme_court_justice, lawyer,
-%% 		   drug_dealer, madam, hired_burgler, hacker, honey_trap, 'PR strategist',
-%% 		   militia_member ]).
-		   
+%
+% Roles
+% 
 roles_relation(secret_parent/unknowing_child).
 
 roles_relation(bounty_hunter/target).
@@ -20,6 +16,7 @@ roles_relation(master/apprentice).
 implies(role(X,master),role(X,force_sensitive)).
 implies(role(X,apprentice),role(X,force_sensitive)).
 contradiction(needs(X,realize_potential_as_jedi),role(X,master)).
+left_unique(master/apprentice).
 
 roles_relation(sith_lord/evil_minion).
 implies(role(X, sith_lord),
@@ -36,6 +33,8 @@ implies(role(X, rebel_soldier),
 	role(X, rebel)).
 implies(role(X, rebel_commander),
 	role(X, rebel)).
+left_unique(rebel_commander/rebel_soldier).
+conflicting_roles([rebel_commander,rebel_soldier,stormtroopers_in_same_unit,sith_lord,master]).
 
 roles_relation(droid/owner).
 conflicting_roles(droid,secret_parent).
@@ -44,6 +43,8 @@ conflicting_roles(droid,apprentice).
 conflicting_roles(droid,sith_lord).
 conflicting_roles(droid,stormtroopers_in_same_unit).
 conflicting_roles(droid,alien_crime_lord).
+conflicting_roles(droid,unknowing_child).
+right_unique(droid/owner).
 
 symmetric(stormtroopers_in_same_unit).
 implies(relationship(X, stormtroopers_in_same_unit, _),
@@ -51,6 +52,7 @@ implies(relationship(X, stormtroopers_in_same_unit, _),
 implies(relationship(_, stormtroopers_in_same_unit, Y),
 	role(Y, imperial)).
 conflicting_roles(stormtroopers_in_same_unit,rebel).
+conflicting_roles(stormtroopers_in_same_unit,sith_lord).
 		
 roles_relation(force_sensitive/skeptic_of_the_force).
 conflicting_roles(force_sensitive,skeptic_of_the_force).
@@ -64,6 +66,8 @@ conflicting_roles(imperial,alien_crime_lord).
 conflicting_roles(rebel,alien_crime_lord).
 
 roles_relation(alien_crime_lord/bounty_hunter).
+conflicting_roles(alien_crime_lord,bounty_hunter).
+
 roles_relation(clumsy_galaxy_food_deliverer/customer).
 conflicting_roles(sith_lord,clumsy_galaxy_food_deliverer).
 conflicting_roles(master,clumsy_galaxy_food_deliverer).
@@ -84,17 +88,23 @@ need(bring_balance_to_the_force).
 contradiction(needs(X,bring_balance_to_the_force),needs(X,crush_the_rebellion)).
 
 need(crush_the_rebellion).
+
 role_need(imperial, crush_the_rebellion).
+
 role_need(apprentice, realize_potential_as_jedi).
+
 role_need(rebel, deliver_secret_message_to_rebels).
+
 role_need(target, escape_from_old_debts_to_crime_lord).
 
 need(destroy_death_star).
 contradiction(needs(C, destroy_death_star),role(C, imperial)).
+
 role_need(bounty_hunter,hunt_for_bounty).
 
 need(make_quick_and_dirty_fortune).
 contradiction(needs(C, destroy_death_star),role(C, sith_lord)).
+
 role_need(sith_lord,convert_everyone_to_dark_side).
 
 
@@ -113,6 +123,9 @@ implies(has(X,controls_to_a_planet_destroying_laser), at(X,inside_the_death_star
 object(bag_of_live_midi-chlorians).
 object(container_of_bacta).
 
+%
+% Locations
+%
 location(inside_the_death_star).
 location(middle_of_an_asteroid_field).
 location(stranded_on_dagobah).
